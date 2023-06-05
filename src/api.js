7dcfedb98cad8559750ce1f3aa4b2133b2ec12ed
Gennadii Shpreer <intellegento@gmail.com>
2023-06-05 14:59:29 +0600

    feat: add icons and fetch for weather

--- src/api.js
+++ src/api.js
@@ -8,3 +8,6 @@ export const geoAPIOptions = {
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "00c18efd8bc1df93940bcffbe107c8ae"
+
--- src/components/currentWeather/index.js
+++ src/components/currentWeather/index.js
@@ -1,9 +1,39 @@
 import "./style.css";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
     return (
         <div className="weather">
-
+            <div className="top">
+                <div>
+                    <p className="city">{data.city}</p>
+                    <p className="wheather-descrription">{data.weather[0].description}</p>
+                </div>
+                <img alt="wheather" className="wheather-icon" src={`icons/${data.weather[0].icon}.svg`} />
+            </div>
+            <div className="bottom">
+                <p className="temperature">18℃</p>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like</span>
+                        <span className="parameter-value">22℃</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">2 km/h</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">15 %</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">15 hPa</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
